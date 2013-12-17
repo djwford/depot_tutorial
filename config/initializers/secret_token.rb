5f82a66107bc5e3efe1ca634d7d3d4e7144fc5e7
Derek <derekweatherford@gmail.com>
2013-12-17 13:30:45 -0700

    initial commit. add scaffold and product model. Add some styles to products index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '6c1fa528904394e8f27a49ee08b244bae6b0e7a02ca26733a8f8e6f6114cab8d61b429e86725f518e6f8d1826fe16c7f4a6124baf338aa03cefe49e8d0cc3095'
